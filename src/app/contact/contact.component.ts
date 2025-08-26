3c4f959472cf01597e87f000e424088fafe3cecd
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-08-26 11:02:47 +0200

    implemented contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule], 
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    nameControl: new FormControl('', Validators.required),
+    emailControl: new FormControl('', [Validators.required, Validators.email]),
+    messageControl: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(20)]),
+  })
+ 
+
+  submitForm() {
+    console.log(this.contactForm.valid);
+    console.log(this.contactForm.get('messageControl')?.errors)
+    // if(this.nameControl.dirty){
+    //   alert('You changed the name field');
+    // }
+    
+  }
 
 }
