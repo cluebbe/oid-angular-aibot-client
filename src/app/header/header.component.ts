8e4bd1534374ba7c86400996b327b8b6123de986
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-08-26 11:24:02 +0200

    added login link to navbar and hid chat link when logged out

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(public authService: AuthService) { }
 
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+    window.location.reload();
+  }
 }
